59027a9c83346505c2839c2d8bb4df7f51492c2c
Anh Duy <anhduykg3k@gmail.com>
2023-11-01 15:14:33 +0700

    first commit

---
+++ buoi9/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace buoi9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnKN = new System.Windows.Forms.Button();
+            this.btnHKN = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.LabMa = new System.Windows.Forms.Label();
+            this.labTen = new System.Windows.Forms.Label();
+            this.txtMa = new System.Windows.Forms.TextBox();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // btnKN
+            // 
+            this.btnKN.Location = new System.Drawing.Point(73, 188);
+            this.btnKN.Name = "btnKN";
+            this.btnKN.Size = new System.Drawing.Size(75, 23);
+            this.btnKN.TabIndex = 0;
+            this.btnKN.Text = "Connect";
+            this.btnKN.UseVisualStyleBackColor = true;
+            this.btnKN.Click += new System.EventHandler(this.btnKN_Click);
+            // 
+            // btnHKN
+            // 
+            this.btnHKN.Location = new System.Drawing.Point(154, 188);
+            this.btnHKN.Name = "btnHKN";
+            this.btnHKN.Size = new System.Drawing.Size(75, 23);
+            this.btnHKN.TabIndex = 1;
+            this.btnHKN.Text = "Disconnet";
+            this.btnHKN.UseVisualStyleBackColor = true;
+            this.btnHKN.Click += new System.EventHandler(this.btnHKN_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(29, 217);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 2;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(116, 217);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 3;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(197, 217);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 4;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // LabMa
+            // 
+            this.LabMa.AutoSize = true;
+            this.LabMa.Location = new System.Drawing.Point(44, 46);
+            this.LabMa.Name = "LabMa";
+            this.LabMa.Size = new System.Drawing.Size(42, 13);
+            this.LabMa.TabIndex = 5;
+            this.LabMa.Text = "Ma mh:";
+            // 
+            // labTen
+            // 
+            this.labTen.AutoSize = true;
+            this.labTen.Location = new System.Drawing.Point(44, 72);
+            this.labTen.Name = "labTen";
+            this.labTen.Size = new System.Drawing.Size(46, 13);
+            this.labTen.TabIndex = 6;
+            this.labTen.Text = "Ten mh:";
+            // 
+            // txtMa
+            // 
+            this.txtMa.Location = new System.Drawing.Point(92, 43);
+            this.txtMa.Name = "txtMa";
+            this.txtMa.Size = new System.Drawing.Size(100, 20);
+            this.txtMa.TabIndex = 7;
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(92, 72);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(100, 20);
+            this.txtTen.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.txtMa);
+            this.Controls.Add(this.labTen);
+            this.Controls.Add(this.LabMa);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.btnHKN);
+            this.Controls.Add(this.btnKN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button btnHKN;
+        private System.Windows.Forms.Button btnKN;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.TextBox txtMa;
+        private System.Windows.Forms.Label labTen;
+        private System.Windows.Forms.Label LabMa;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+    }
+}
+
---
+++ buoi9/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace buoi9
+{
+    public partial class Form1 : Form
+    {
+        string strCon = @"Data Source=A209PC31\CSSQL08;Initial Catalog=test;Integrated Security=True";
+        SqlConnection sqlCon = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(sqlCon==null)
+                {
+                    sqlCon = new SqlConnection(strCon);
+                }
+                if(sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                    MessageBox.Show("Kết nối thành công");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnHKN_Click(object sender, EventArgs e)
+        {
+            if (sqlCon != null && sqlCon.State == ConnectionState.Open)
+            {
+                sqlCon.Close();
+                MessageBox.Show("Đóng kết nối thành công");
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                string insertString;
+                insertString = "insert into KhoaHoc values('" + txtMa.Text + "',N'" + txtTen.Text + "')";
+                SqlCommand cmd = new SqlCommand(insertString, sqlCon);
+                cmd.ExecuteNonQuery();
+                if (sqlCon != null && sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                    MessageBox.Show("Đóng kết nối thành công");
+                }
+                MessageBox.Show("Thêm thành công");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Thêm thất bại");
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                string updateString;
+                updateString = "update KhoaHoc set TenMH='" + txtTen.Text + "'where MaMH='" + txtMa + "'";
+                SqlCommand cmd = new SqlCommand(updateString, sqlCon);
+                cmd.ExecuteNonQuery();
+                if (sqlCon != null && sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+                MessageBox.Show("Update thành công");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Update thất bại");
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                }
+                string deleteString;
+                deleteString = "delete KhoaHoc where MaMH='" + txtMa.Text +"'";
+                SqlCommand cmd = new SqlCommand(deleteString, sqlCon);
+                cmd.ExecuteNonQuery();
+                if (sqlCon != null && sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+                MessageBox.Show("Xóa thành công");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Xóa thất bại");
+            }
+        }
+    }
+}
